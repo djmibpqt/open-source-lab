4de444f8b41430353263889a6143f4ab757e3154
DJ Mib <DJ Mib>
2016-01-24 06:11:55 -0200

    Listando Projetos e Participantes

--- index.js
+++ index.js
@@ -7,3 +7,31 @@
  * [] Printar todos os projeto - participante
  */
 
+var projetos = [
+    "Open Source",
+    "HELabs",
+    "NegoBan"
+];
+
+var participantes = [
+    "Bernardo da Costa Santos",
+    "Cayo Medeiros (yogodoshi)",
+    "Diego Santos Araújo",
+    "Guilherme Fernandes Souza da Paixão",
+    "Hugo Raphael Vianna Amorim",
+    "José Felipe dos Santos Lima",
+    "Mateus Paulino",
+    "Ronalson Vargas Mendes Filho"
+];
+
+function listarProjetos(){
+    for(var projeto in projetos){
+        console.log(projetos[projeto] + "\n");
+    }
+}
+
+function listarParticipantes(){
+    for(var participante in participantes){
+        console.log(participantes[participante] + "\n");
+    }
+}
